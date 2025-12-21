6f7c7649418f3cc966aaffecc3e08df7466d398a
BinaryOxide <234583143+BinaryOxide@users.noreply.github.com>
2025-12-21 23:26:11 +0600

    update: I tried to implement connection betwenn c++ and json to see how it works
    key: I got basics idea of implementation

--- cpp_json_link/cpp_json_link.cpp
+++ cpp_json_link/cpp_json_link.cpp
@@ -1,9 +1,17 @@
-
-
 #include <iostream>
+#include "third_party/nlohmann/json.hpp"
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using json = nlohmann::json;
+
+int main() {
+    json config;
+
+    config["print_hello"] = false;
+    config["hello_color"] = "red";
+
+    if (config["print_hello"]) {
+        std::cout << "Hello World\n";
+    }
+
+    return 0;
 }
-
