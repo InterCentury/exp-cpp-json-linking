270e191199a69716faf721c00162ae3f018e9ec7
BinaryOxide <234583143+BinaryOxide@users.noreply.github.com>
2025-12-21 23:41:48 +0600

    test: tried to implement color with c++ json
    result: it's working fine

--- cpp_json_link/cpp_json_link.cpp
+++ cpp_json_link/cpp_json_link.cpp
@@ -3,14 +3,26 @@
 
 using json = nlohmann::json;
 
+std::string colorCode(const std::string& color) {
+    if (color == "red")    return "\033[31m";
+    if (color == "green")  return "\033[32m";
+    if (color == "yellow") return "\033[33m";
+    if (color == "blue")   return "\033[34m";
+    if (color == "reset")  return "\033[0m";
+    return "\033[0m"; // default
+}
+
 int main() {
     json config;
 
-    config["print_hello"] = false;
+    config["print_hello"] = true;
     config["hello_color"] = "red";
 
     if (config["print_hello"]) {
-        std::cout << "Hello World\n";
+        std::string color = config["hello_color"];
+        std::cout << colorCode(color)
+            << "Hello World"
+            << colorCode("reset") << "\n";
     }
 
     return 0;
